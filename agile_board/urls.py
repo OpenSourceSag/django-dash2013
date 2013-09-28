11bf7969362223b389df5f0478851dd519d60dcf
Alexandre Lessard <belug@oss.cx>
2013-09-28 15:36:25 +0000

    Adding the sprint view

--- agile_board/urls.py
+++ agile_board/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from scrum.views import WhiteBoardView, add_story
+from scrum.views import SprintView
 
 
 
@@ -11,14 +12,14 @@ urlpatterns = patterns('',
     # Examples:
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    
-    url(r'^(?P<pk>[-_\w]+)/$', WhiteBoardView.as_view(), name='whiteboard'),
-    url(r'^[0-9]+/story/add/$', add_story, name='whiteboard'),
 
+    url(r'^sprint/$', SprintView.as_view(), name='sprint'),
+    url(r'^project/(?P<pk>[0-9]+)/$', WhiteBoardView.as_view(), name='whiteboard'),
+    url(r'^project/[0-9]+/story/add/$', add_story, name='whiteboard'),
     # url(r'^agile_board/', include('agile_board.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    
+
 )
--- scrum/views.py
+++ scrum/views.py
@@ -5,14 +5,14 @@ from django.http import HttpResponseNotFound, HttpResponse
 import string
 import json
 
-class WhiteBoardView(TemplateView):
-    template_name = 'scrum/whiteboard.html'
+class SprintView(TemplateView):
+    template_name = 'scrum/sprint.html'
 
 
 class WhiteBoardView(DetailView):
     template_name = 'scrum/whiteboard.html'
     model = Project
-    
+
     def get_context_data(self, **kwargs):
         # Call the base implementation first to get a context
         context = super(WhiteBoardView, self).get_context_data(**kwargs)
@@ -21,23 +21,23 @@ class WhiteBoardView(DetailView):
         project_stories = Story.objects.filter(project=project)
         project_tasks = Task.objects.filter(story__in=project_stories)
         project_tasks_backlog = project_tasks.filter(status='BACKLOGS')
-        
+
         context['stories'] = project_stories
         context['tasks'] = project_tasks
         context['tasks_backlog'] = project_tasks_backlog
-        
+
         return context
-    
+
 def add_story(request):
-    
+
     if request.method == 'POST':
         url = request.META.get('PATH_INFO')[1:]
         project_id = url[:url.find('/')]
-        
+
         story_title = request.POST.get('title')
         story_time = request.POST.get('time')
         story_project = Project.objects.get(pk=project_id)
-        
+
         new_story = Story.objects.create(title = story_title,
                                          project = story_project ,
                                          estimated_time = story_time)
@@ -45,10 +45,10 @@ def add_story(request):
         response_data = {}
         response_data['story_pk'] = new_story.pk
         return HttpResponse(json.dumps(response_data), content_type="application/json")
-    
+
     else:
         return HttpResponseNotFound('<h1> Not Found </h1>')
     #return response
-    
-    
-    
+
+
+
