44fd565ad73e2b1c6a9f956a3924f25c1fb9b17b
Alexandre Lessard <belug@oss.cx>
2013-09-28 02:58:48 +0000

    requirements and south

--- agile_board/settings/base.py
+++ agile_board/settings/base.py
@@ -128,6 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'south',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
