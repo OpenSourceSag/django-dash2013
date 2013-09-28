94fa9b92fc4eedf9c5b0f54fcd889618c2e33f2c
Franck <enjoysunfire@hotmail.fr>
2013-09-28 15:23:55 +0000

    adding story from board

--- agile_board/urls.py
+++ agile_board/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from scrum.views import WhiteBoardView
+from scrum.views import WhiteBoardView, add_story
 
 
 
@@ -13,6 +13,8 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     
     url(r'^(?P<pk>[-_\w]+)/$', WhiteBoardView.as_view(), name='whiteboard'),
+    url(r'^[0-9]+/story/add/$', add_story, name='whiteboard'),
+
     # url(r'^agile_board/', include('agile_board.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
--- scrum/views.py
+++ scrum/views.py
@@ -1,6 +1,9 @@
 from django.views.generic import TemplateView
 from django.views.generic.detail import DetailView
 from .models import Project, Story, Task, Sprint, SprintTasks
+from django.http import HttpResponseNotFound, HttpResponse
+import string
+import json
 
 class WhiteBoardView(TemplateView):
     template_name = 'scrum/whiteboard.html'
@@ -17,9 +20,35 @@ class WhiteBoardView(DetailView):
         project = kwargs.get('object')
         project_stories = Story.objects.filter(project=project)
         project_tasks = Task.objects.filter(story__in=project_stories)
-        
+        project_tasks_backlog = project_tasks.filter(status='BACKLOGS')
         
         context['stories'] = project_stories
         context['tasks'] = project_tasks
+        context['tasks_backlog'] = project_tasks_backlog
         
         return context
+    
+def add_story(request):
+    
+    if request.method == 'POST':
+        url = request.META.get('PATH_INFO')[1:]
+        project_id = url[:url.find('/')]
+        
+        story_title = request.POST.get('title')
+        story_time = request.POST.get('time')
+        story_project = Project.objects.get(pk=project_id)
+        
+        new_story = Story.objects.create(title = story_title,
+                                         project = story_project ,
+                                         estimated_time = story_time)
+
+        response_data = {}
+        response_data['story_pk'] = new_story.pk
+        return HttpResponse(json.dumps(response_data), content_type="application/json")
+    
+    else:
+        return HttpResponseNotFound('<h1> Not Found </h1>')
+    #return response
+    
+    
+    
