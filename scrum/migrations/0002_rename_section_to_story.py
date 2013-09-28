e05caf85afe43466a2b399f3fe43e84614e01629
Franck <enjoysunfire@hotmail.fr>
2013-09-28 13:51:14 +0000

    change to model

---
+++ scrum/migrations/0002_rename_section_to_story.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        db.rename_table('scrum_section', 'scrum_story')
+
+    def backwards(self, orm):
+        db.rename_table('scrum_story', 'scrum_section')
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'scrum.project': {
+            'Meta': {'ordering': "('id',)", 'object_name': 'Project'},
+            'description': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'}),
+            'users': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'Project_users'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['auth.User']"})
+        },
+        u'scrum.section': {
+            'Meta': {'ordering': "('title',)", 'object_name': 'Section'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'Section_project'", 'to': u"orm['scrum.Project']"}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '255'})
+        },
+        u'scrum.sprint': {
+            'Meta': {'ordering': "('number',)", 'unique_together': "(('number', 'project'),)", 'object_name': 'Sprint'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_closed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'number': ('django.db.models.fields.IntegerField', [], {}),
+            'project': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'Sprint_project'", 'to': u"orm['scrum.Project']"}),
+            'tasks': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'to': u"orm['scrum.Task']", 'null': 'True', 'through': u"orm['scrum.SprintTasks']", 'blank': 'True'})
+        },
+        u'scrum.sprinttasks': {
+            'Meta': {'object_name': 'SprintTasks'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'sprint': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'sprints'", 'to': u"orm['scrum.Sprint']"}),
+            'task': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'tasks'", 'to': u"orm['scrum.Task']"}),
+            'task_end_status': ('django.db.models.fields.CharField', [], {'max_length': '10'})
+        },
+        u'scrum.task': {
+            'Meta': {'ordering': "('id',)", 'object_name': 'Task'},
+            'assigned_to': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'Task_users'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['auth.User']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_modified': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'note': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'Task_section'", 'to': u"orm['scrum.Section']"}),
+            'status': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        }
+    }
+
+    complete_apps = ['scrum']
--- scrum/models.py
+++ scrum/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 TASKS_STATUS = (('TODO', 'To do'),
                 ('INPROGRESS', 'In progress'),
@@ -22,11 +23,12 @@ class Project(models.Model):
         return "%s" % (self.name, )
 
 
-class Section(models.Model):
+class Story(models.Model):
     title = models.CharField(unique=True, max_length=255)
     note = models.TextField(blank=True)
-    time = models.DateTimeField(auto_now=True)
-    project = models.ForeignKey(Project, related_name='Section_project')
+    last_modified = models.DateTimeField(auto_now=True)
+    project = models.ForeignKey(Project, related_name='Story_project')
+    estimated_time = models.FloatField(validators = [MinValueValidator(0), MaxValueValidator(5)])
 
     class Meta:
         ordering = ('title',)
@@ -41,7 +43,8 @@ class Task(models.Model):
     status = models.CharField(max_length=10, choices=TASKS_STATUS)
     last_modified = models.DateTimeField(auto_now=True)
     assigned_to = models.ManyToManyField(User, related_name='Task_users', blank=True, null=True)
-    section = models.ForeignKey(Section, related_name='Task_section')
+    story = models.ForeignKey(Story, related_name='Task_story')
+    estimated_time = models.FloatField(validators = [MinValueValidator(0), MaxValueValidator(8)])
 
     class Meta:
         ordering = ('id',)
@@ -56,6 +59,7 @@ class Sprint(models.Model):
     number = models.IntegerField()
     is_closed = models.BooleanField()
     project = models.ForeignKey(Project, related_name='Sprint_project')
+    last_modified = models.DateTimeField(auto_now=True)
     tasks = models.ManyToManyField(Task, through='SprintTasks', blank=True, null=True)
 
     class Meta:
