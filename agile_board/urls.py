5537cc13df40394fb707f5d59b83abee29878fe9
Alexandre Lessard <belug@oss.cx>
2013-09-29 18:17:00 +0000

    Again trying to setup statics

--- agile_board/urls.py
+++ agile_board/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
-from django.conf import settings
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from scrum.views import *
 
@@ -29,11 +27,3 @@ urlpatterns = patterns('',
     url(r'^login/?$', 'django.contrib.auth.views.login', {'template_name': 'scrum/registration/login.html', }, name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'template_name': 'scrum/registration/logged_out.html', }, name='logout'),
 )
-
-urlpatterns += patterns('',
-    (r'^static/(.*)$', 'django.views.static.serve', {
-        'document_root': settings.STATIC_ROOT
-    }),
-)
-
-urlpatterns += staticfiles_urlpatterns()
