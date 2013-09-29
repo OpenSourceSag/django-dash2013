58c37e8860079591c81c8c26d1b202b836acbee1
Alexandre Lessard <belug@oss.cx>
2013-09-29 17:30:51 +0000

    Adding heroku name to allowed hosts

--- agile_board/settings/base.py
+++ agile_board/settings/base.py
@@ -30,7 +30,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['agile-wall.oss.cx']
+ALLOWED_HOSTS = ['agile-wall.oss.cx', 'ossdjangodash2013.herokuapp.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
